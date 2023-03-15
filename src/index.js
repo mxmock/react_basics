11fea3756a7527c987c43d9bb13dfef290c0bd09
mxmock <oc.kode@gmail.com>
2023-03-15 10:27:30 +0100

    Handle generic style (index.scss)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.scss";
 import App from "./components/app/App";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
